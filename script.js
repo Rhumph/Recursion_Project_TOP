e7226b3822276541d8cd194d55e4883c63b1dfdd
Night City <rileyjhumphreys@gmail.com>
2024-09-21 15:45:11 +1000

    Completed mergeSort Algorithim

--- script.js
+++ script.js
@@ -11,20 +11,55 @@ function fibs(number) {
     }
   }
 
-return fibsArray
+  return fibsArray;
 }
 
 // Fibonnacci sequence using recursion
-function fibsRec(number, fibArray = [0, 1]){ 
+function fibsRec(number, fibArray = [0, 1]) {
+  if (fibArray.length >= number) return fibArray;
 
-    if (fibArray.length >= number)
-        return fibArray
+  const nextFib = fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2];
+  fibArray.push(nextFib);
 
-    const nextFib = fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2];
-    fibArray.push(nextFib);
-
-    return fibsRec(number, fibArray);
+  return fibsRec(number, fibArray);
 }
 
-fibs(12);
-console.log(fibsRec(8))
+// Merge Sort using Recursion
+function mergeSort(array) {
+    if (array.length <= 1) {
+      return array;
+    }
+  
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+  
+    const merge = (left, right) => {
+      let resultArray = [];
+      let leftIndex = 0;
+      let rightIndex = 0;
+  
+      while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+          resultArray.push(left[leftIndex]);
+          leftIndex++;
+        } else {
+          resultArray.push(right[rightIndex]);
+          rightIndex++;
+        }
+      }
+  
+      return resultArray
+              .concat(left.slice(leftIndex))
+              .concat(right.slice(rightIndex));
+    };
+  
+    return merge(mergeSort(left), mergeSort(right));
+  }
+  
+  const array = [34, 7, 23, 32, 5, 62];
+  const sortedArray = mergeSort(array);
+  console.log(sortedArray);
+
+// fibs(12);
+// console.log(fibsRec(8));
